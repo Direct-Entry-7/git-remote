9f893d4b3b7c68182bcfcbcc278d16d789dc0d1e
Gayal Seneviratne <gayals40@gmail.com>
2021-06-02 12:14:49 +0530

    Add New lines in AppInitializer

--- AppInitializer.java
+++ AppInitializer.java
@@ -1,5 +1,7 @@
 public class AppInitializer{
     public static void main(String args[]){
         System.out.println("Hello Git Demos!");
+        System.out.println("sdssds!");
+        System.out.println("ddddd!");
     }
 }
